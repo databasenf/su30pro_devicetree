852d208f620d18dd03cdbb69321ac582c9545495
databasenf <3326637451@qq.com>
2026-05-01 18:05:07 +0800

    Update path for prebuilt DTB image

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,7 +62,7 @@ TARGET_KERNEL_SOURCE := kernel/iflytek/su30pro
 TARGET_FORCE_PREBUILT_KERNEL := true
 ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
 TARGET_PREBUILT_KERNEL := device/iflytek/su30pro/prebuilt/kernel
-BOARD_PREBUILT_DTBIMAGE := device/iflytek/su30pro/prebuilt/dtb.img
+BOARD_PREBUILT_DTBIMAGE := $(PRODUCT_OUT)/dtb.img
 BOARD_PREBUILT_DTBOIMAGE := device/iflytek/su30pro/prebuilt/dtbo.img
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
