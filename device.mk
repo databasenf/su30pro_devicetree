25d83f3d14d52b43d21cb167bb8117318f74eccd
databasenf <3326637451@qq.com>
2026-05-01 18:04:03 +0800

    Update device.mk to include dtb.img copy instruction
    Added instruction to copy dtb.img and updated recovery.fstab path.

--- device.mk
+++ device.mk
@@ -1,8 +1,9 @@
 LOCAL_PATH := device/iflytek/su30pro
 
-# 拷贝 recovery.fstab
+# 单独对 dtb.img 增加拷贝指令
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery.fstab:$(TARGET_COPY_OUT_RECOVERY)/root/system/etc/recovery.fstab
+    device/iflytek/su30pro/prebuilt/dtb.img:dtb.img \
+    device/iflytek/su30pro/recovery.fstab:$(TARGET_COPY_OUT_RECOVERY)/root/system/etc/recovery.fstab
 
 # 声明动态分区
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
