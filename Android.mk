ed75a6aeadb491dd92830ad1ce4a53cd7c9507e2
databasenf <3326637451@qq.com>
2026-05-01 17:31:58 +0800

    Refactor Android.mk to include prebuilt modules

--- Android.mk
+++ Android.mk
@@ -2,18 +2,28 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),su30pro)
 
-# 定义一个内部函数来处理拷贝
-define copy-prebuilt-file
-$(eval $(PRODUCT_OUT)/$(2): $(LOCAL_PATH)/prebuilt/$(1)
-	@echo "Copying $(1) -> $(2)"
-	@mkdir -p $$(dir $$@)
-	@cp -fp $$< $$@
-)
-endef
+include $(CLEAR_VARS)
+LOCAL_MODULE := dtbo_prebuilt
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES := prebuilt/dtbo.img
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)
+LOCAL_MODULE_STEM := dtbo.img
+include $(BUILD_PREBUILT)
 
-# 执行拷贝动作：(源码文件名, 目标文件名)
-$(eval $(call copy-prebuilt-file,kernel,kernel))
-$(eval $(call copy-prebuilt-file,dtb.img,dtb.img))
-$(eval $(call copy-prebuilt-file,dtbo.img,dtbo.img))
+include $(CLEAR_VARS)
+LOCAL_MODULE := dtb_prebuilt
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES := prebuilt/dtb.img
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)
+LOCAL_MODULE_STEM := dtb.img
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := kernel_prebuilt
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES := prebuilt/kernel
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)
+LOCAL_MODULE_STEM := kernel
+include $(BUILD_PREBUILT)
 
 endif
