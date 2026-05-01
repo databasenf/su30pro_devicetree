ac3e8e2cb473bc15fdd0505ecf1d4f371a67c258
databasenf <3326637451@qq.com>
2026-05-01 17:50:46 +0800

    Clean up device.mk by removing unused comments
    Removed commented lines for product packages.

--- device.mk
+++ device.mk
@@ -1,11 +1,5 @@
 LOCAL_PATH := device/iflytek/su30pro
 
-# 在 device/iflytek/su30pro/device.mk 中加入
-PRODUCT_PACKAGES += \
-    dtbo_prebuilt \
-    dtb_prebuilt \
-    kernel_prebuilt
-
 # 拷贝 recovery.fstab
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery.fstab:$(TARGET_COPY_OUT_RECOVERY)/root/system/etc/recovery.fstab
