4939a34720b280bf30b398f30ffd9501ba932aa3
databasenf <3326637451@qq.com>
2026-05-01 12:40:44 +0800

    Update device.mk for recovery and dynamic partitions

--- device.mk
+++ device.mk
@@ -1,8 +1,19 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
+# 拷贝 recovery.fstab
+# 注意：Android 11+ 的 Recovery 路径通常是在 /system/etc/
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery.fstab:$(TARGET_COPY_OUT_RECOVERY)/root/system/etc/recovery.fstab
 
-LOCAL_PATH := device/iflytek/su30pro
+# 如果你有 dtbo.img 且需要包含
+ifeq ($(TARGET_PREBUILT_DTBOIMAGE),)
+    # 自动搜索 dtbo
+endif
+
+# 声明动态分区支持
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# 开启 AOSP 的一些基础特性
+PRODUCT_PACKAGES += \
+    otafault \
+    ext4_utils \
+    e2fsck_static \
+    mke2fs_static
