6c4d8e8067b46c1580fa61c9eb65cd84dd9cd629
databasenf <3326637451@qq.com>
2026-05-01 12:30:29 +0800

    Add omni_su30pro.mk for device configuration

---
+++ omni_su30pro.mk
@@ -0,0 +1,22 @@
+# 继承 TWRP 通用配置
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# 继承 AOSP 基础配置
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# 包含你的设备特定配置
+$(call inherit-product, device/iflytek/su30pro/device.mk)
+
+# 产品定义
+PRODUCT_DEVICE := su30pro
+PRODUCT_NAME := omni_su30pro
+PRODUCT_BRAND := iflytek
+PRODUCT_MODEL := SU30PRO SSD V12
+PRODUCT_MANUFACTURER := iflytek
+
+# 设置编译指纹（可以随便填，但建议参考原厂 build.prop）
+PRODUCT_GMS_CLIENTID_BASE := iFLYTEK/su30pro/su30pro:12/SQ3A.220605.009.B1/root01072119:user/release-keys
+
+# 强制指定显示语言
+PRODUCT_DEFAULT_LOCALE := zh_CN
