ed471142e0ba5023aaf4179b0f53e781f63cdc04
databasenf <3326637451@qq.com>
2026-05-20 21:16:41 +0800

    更新 BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,17 +10,20 @@ DEVICE_PATH := device/iflytek/su30pro
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 
-# --- 架构与内核基址 ---
+# --- CPU 架构与指令集定义 ---
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 := 
 TARGET_CPU_VARIANT := generic
 
-# 必须声明二次架构（32位支持），否则 AOSP 会认为配置不完整
+# --- 兼容 32 位支持（很多 Android 12 底层依赖需要） ---
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
+
 
 # 显式开启 64 位支持
 TARGET_IS_64_BIT := true
